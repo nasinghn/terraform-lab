92d12c5c8eeb0399811050cf8af4b5b9e0c4afad
nasinghn <33350388+nasinghn@users.noreply.github.com>
2023-06-15 12:30:34 +0530

    added new variable

--- main.tf
+++ main.tf
@@ -10,7 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "app_server" {
